3177423658235b0c4db6940292d3cdaffe2a8c3f
Maksym Sabov <maksim50.97@gmail.com>
2025-04-08 01:00:02 +0300

    Final project setup: structure, theme, helpers, configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,54 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { App } from './App';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  ChakraProvider,
+  createSystem,
+  defaultConfig,
+  defineConfig,
+} from '@chakra-ui/react';
+
+const config = defineConfig({
+  theme: {
+    tokens: {
+      colors: {
+        sky: {
+          500: { value: '63B3ED' },
+        },
+        gray: {
+          50: { value: 'F7FAFC' },
+          700: { value: 'F7FAFC' },
+        },
+      },
+      breakpoints: {
+        sm: { value: '30em' },
+        md: { value: '48em' },
+        lg: { value: '62em' },
+        xl: { value: '80em' },
+      },
+    },
+  },
+  globalCss: {
+    'html, body': {
+      margin: 0,
+      padding: 0,
+      boxSizing: 'border-box',
+      backgroundColor: 'gray.50',
+      color: 'gray.700',
+      fontFamily: "'Inter', sans-serif",
+    },
+    '*, *:before, *:after': {
+      boxSizing: 'inherit',
+    },
+  },
+});
+
+const system = createSystem(defaultConfig, config);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ChakraProvider value={system}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
---
+++ src/types/weather.ts
@@ -0,0 +1,11 @@
+export interface WeatherData {
+  name: string;
+  main: {
+    temp: number;
+  };
+  weather: {
+    description: string;
+    icon: string;
+  };
+  dt: number;
+}
